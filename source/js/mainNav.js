562c02bd689d72830b2a564620b3a882122a9ea1
Anton Malykhin <a.y.malykhin@gmail.com>
2020-01-14 18:38:26 +0300

    Вносит исправления

--- source/js/mainNav.js
+++ source/js/mainNav.js
@@ -1,10 +1,21 @@
 var mainMenu = document.querySelector(".main-nav");
+var innerPageMenu = document.querySelector(".inner-page__nav");
 var menuButton = document.querySelector(".main-nav__toggle");
 
 menuButton.addEventListener("click", function() {
-  if (mainMenu.classList.contains("main-nav--closed")) {
-    mainMenu.classList.remove("main-nav--closed");
+  if (innerPageMenu) {
+    if (mainMenu.classList.contains("main-nav--closed") &&        innerPageMenu.classList.contains("inner-page__nav--closed")) {
+      mainMenu.classList.remove("main-nav--closed");
+      mainMenu.classList.remove("inner-page__nav--closed");
+    } else {
+      mainMenu.classList.add("main-nav--closed");
+      mainMenu.classList.add("inner-page__nav--closed"); 
+    }
   } else {
-    mainMenu.classList.add("main-nav--closed");
+    if (mainMenu.classList.contains("main-nav--closed")) {
+      mainMenu.classList.remove("main-nav--closed");
+    } else {
+      mainMenu.classList.add("main-nav--closed"); 
+    }
   }
 });
